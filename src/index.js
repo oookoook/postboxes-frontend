9bd3c3ddbb4e143019c3e4e3bdb0805713378d6f
Adam Kučera <akucera@mail.muni.cz>
2019-02-04 23:38:26 +0100

    clean npm script added

--- src/index.js
+++ src/index.js
@@ -33,7 +33,6 @@ var app = new Vue({
   store,
   render: h => h(App),
     mounted () {
-    // You'll need this for renderAfterDocumentEvent.
     document.dispatchEvent(new Event('render-event'));
   }
 });
